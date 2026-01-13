0691f740a6e014440fa650b7c386d0020b50bf4c
Mark <mark67769@gmail.com>
2026-01-13 16:32:56 +0200

    Login and list products

---
+++ src/test/java/markyurievqa/EndToEnd.java
@@ -0,0 +1,38 @@
+package markyurievqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class EndToEnd {
+    @Test
+    public void oneBigTest() throws InterruptedException {
+        String email = "mark2222@gmail.com";
+        String password = "Minecraft158!";
+
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+
+        driver.get("https://rahulshettyacademy.com/client");
+
+        driver.findElement(By.id("userEmail")).sendKeys(email);
+        driver.findElement(By.id("userPassword")).sendKeys(password);
+        driver.findElement(By.id("login")).click();
+
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".card-body b")));
+
+        List<WebElement> products = driver.findElements(By.cssSelector(".card"));
+        products.stream().map(x -> x.findElement(By.cssSelector(".card-body b")).getText()).forEach(System.out::println);
+
+        
+    }
+
+}
